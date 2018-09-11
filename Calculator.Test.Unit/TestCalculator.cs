6ee92860a3bbe4a606147d0f1d1ed298c9e51191
HiroshikunDK <36033304+HiroshikunDK@users.noreply.github.com>
2018-09-11 20:02:36 +0200

    test added division
    added a devide by zero test to confim exception handling

--- Calculator.Test.Unit/TestCalculator.cs
+++ Calculator.Test.Unit/TestCalculator.cs
@@ -100,6 +100,11 @@ namespace Calculator.Test.Unit
             Assert.That(_uut.Divide(0, 5), Is.EqualTo(0));
         }
 
+        [Test]
+        public void Divide_Divide5and0ReturnError()
+        {
+            Assert.That(_uut.Divide(0, 5), Is.EqualTo(-1)); 
+        }
         /* Above we've tried using the tag [Test]. We can conclude that the method of using [Test] is rather slow and inefficient to write out. 
            Therefore we continue with the tag [TestCase]. This tag is a lot faster to work with, if you're working with the same method a few times and only need to change the values.
         */
